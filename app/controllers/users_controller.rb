f3054e7a6ecdb7dc4b061efb7a1aa70b895aed2e
David Adams <david.riding.rails@gmail.com>
2019-02-02 18:02:47 -0700

    Update users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  
   def new
     @user = User.new
   end
@@ -6,4 +7,21 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    # @user = User.new(params[:user]) #This may use scary params from the internet
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path
+    else
+      render :new
+    end    
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)    
+    end
+
 end
