b51127a95a73417dd0866064756b922468886b5e
David Adams <david.riding.rails@gmail.com>
2019-02-04 18:43:57 -0700

    Add users login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,12 @@ class UsersController < ApplicationController
     # @user = User.new(params[:user]) #This may use scary params from the internet
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"      
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render :new
-    end    
+    end
   end
 
   private
