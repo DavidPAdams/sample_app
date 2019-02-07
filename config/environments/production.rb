977f1319757b8e5862ef900cddc03c0b8ca002c9
David Adams <david.riding.rails@gmail.com>
2019-02-07 11:48:09 -0700

    Remove SendGrid and add MailGun for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,22 +63,40 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app_#{Rails.env}"
 
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  host = 'peaceful-wave-94346.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
+
+
+
+    # The following section is config from the rails tutorial for using SendGrid
+    # email addon with heroku. I found SendGrid to be SHIT and won't use it
+              # config.action_mailer.raise_delivery_errors = true
+              # config.action_mailer.delivery_method = :smtp
+              # host = 'peaceful-wave-94346.herokuapp.com'
+              # config.action_mailer.default_url_options = { host: host }
+              # ActionMailer::Base.smtp_settings = {
+              #   :address        => 'smtp.sendgrid.net',
+              #   :port           => '587',
+              #   :authentication => :plain,
+              #   :user_name      => ENV['SENDGRID_USERNAME'],
+              #   :password       => ENV['SENDGRID_PASSWORD'],
+              #   :domain         => 'heroku.com',
+              #   :enable_starttls_auto => true
+              # }
+
+
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'peaceful-wave-94346.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
