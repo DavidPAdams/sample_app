096faedc3836401c282f8989333b59ed95cdb126
David Adams <david.riding.rails@gmail.com>
2019-02-07 16:06:53 -0700

    Adjust production email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'peaceful-wave-94346.heroku.com',
+    :domain         => host,
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
