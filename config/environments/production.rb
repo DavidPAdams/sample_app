6b5daba1138fdbd6b16e62ab433734876a826de7
David Adams <david.riding.rails@gmail.com>
2019-02-07 13:02:52 -0700

    Revise production email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,9 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'peaceful-wave-94346.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
