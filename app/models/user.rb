d41fa9f036a8492afd0aabed2c91a1f5f383e88b
David Adams <david.riding.rails@gmail.com>
2019-02-25 09:12:18 -0700

    Update User model and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  has_many :microposts, dependent: :destroy
+
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   self.per_page = 5
