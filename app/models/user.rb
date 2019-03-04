00fdcbe384b5d39cc6db315ad86ab40ae7b64850
David Adams <david.riding.rails@gmail.com>
2019-03-04 15:55:12 -0700

    Add functionality for followed users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,15 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   has_secure_password
 
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed                                  
 
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   self.per_page = 5
 
@@ -67,6 +70,18 @@ class User < ApplicationRecord
 
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  def follow(other_user)
+    following << other_user    
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
