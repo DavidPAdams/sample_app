56d029f9ce48d5fa0b4f9c074edc33d4603187b7
David Adams <david.riding.rails@gmail.com>
2019-03-04 17:47:19 -0700

    Add functionality for followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,8 +18,11 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
-  has_many :following, through: :active_relationships, source: :followed                                  
-
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy                                  
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   self.per_page = 5
 
